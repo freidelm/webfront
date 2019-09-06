2ecdf11570809611f26ef5cdf5cf070a403c7874
ESTUDIANTE <estudiante@ul.edu.co>
2019-09-05 20:25:40 -0500

    Corrigiendo Routing y creando dashboard

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,19 +1,29 @@
 import { NgModule } from "@angular/core";
-import{ Routes,RouterModule, RoutesRecognized } from "@angular/router";
+import {Routes,RouterModule, Router} from "@angular/router";
 import { LoginComponent } from './login/login.component';
+import { ThemeComponent } from './theme/theme.component';
 
-export const routes:Routes=[{
-    path: 'login',
-    component: LoginComponent
-}]
-
-
-@NgModule({
-      imports: [RouterModule.forRoot(routes, {useHash:true})],
-      exports: [],
-      providers: []
-      
-})
-export class AppRoutingModule{
-
+export const routes: Routes=[{
+    path  : 'login',    
+    component : LoginComponent
+},
+{
+    path: 'pages',
+    component: ThemeComponent,
+    children: [
+        {
+            path: '',
+            loadChildren:'./pages/dashboard/dashboard.module#DashboardModule'
+        }
+    ]
 }
+
+
+]
+@NgModule({
+imports:[ RouterModule.forRoot(routes,{useHash:true})
+],
+exports:[RouterModule],
+providers:[]
+}) 
+export class AppRoutingModule {}
